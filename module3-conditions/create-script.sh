f6dc01935f39150e42a27a56270ee25793be34ea
simran <simranpal.singh@suse.com>
2020-05-01 19:34:59 -0700

    add condition to check directory exists

--- module3-conditions/create-script.sh
+++ module3-conditions/create-script.sh
@@ -12,7 +12,20 @@ fi
 bindir="${HOME}/bin"
 filename="${bindir}/$1"
 
+# -e checks file exists
 if [[ -e $filename ]]; then
     echo "File ${filename} already exists"
     exit 1
 fi
+
+#check if bin directory exists
+if [[ ! -d $bindir ]]; then
+    if mkdir "$bindir"; then
+        echo "created ${bindir}"
+    else
+        echo "could not create ${bindir}"
+        exit 1
+    fi
+fi
+
+exit 0
